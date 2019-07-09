5b25d0affd6806aec696fd4c74c9104cc0a6b8ed
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-07-09 15:10:37 +0300

    adding the tests for the markedTodo function

--- test.js
+++ test.js
@@ -6,12 +6,27 @@ test("Example test", function(t) {
   t.end();
 });
 
-
-test("Test Delete Function",function(t){
-
-  var obj=[{id:0 ,description:"Rana",done:true},{id:1,description:"yousef",done:true}];
-  var excpected=[{id:1 ,description:"yousef",done:true}];
-  var actual = logic.deleteTodo(obj,0);
-  t.deepEqual(actual,excpected,"when passing array ,Element should deleted");
+test("Test Delete Function", function(t) {
+  var obj = [
+    { id: 0, description: "Rana", done: true },
+    { id: 1, description: "yousef", done: true }
+  ];
+  var excpected = [{ id: 1, description: "yousef", done: true }];
+  var actual = logic.deleteTodo(obj, 0);
+  t.deepEqual(actual, excpected, "when passing array ,Element should deleted");
   t.end();
-})
+});
+
+test("Test Marked Function", function(t) {
+  var obj = [
+    { id: 0, description: "Rana", done: false },
+    { id: 1, description: "yousef", done: false }
+  ];
+  var actual = logic.markTodo(obj, 0);
+  var excpected = [
+    { id: 0, description: "Rana", done: true },
+    { id: 1, description: "yousef", done: false }
+  ];
+  t.deepEqual(actual, excpected, "Should make the item with id done true");
+  t.end();
+});
