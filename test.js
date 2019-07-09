5eacbbac10caef5dca212015ff31318ce28cf123
rana <ranasobeid95@gmail.com>
2019-07-09 14:33:18 +0300

    adding the test file with test deleting work Relates #4

--- test.js
+++ test.js
@@ -5,3 +5,13 @@ test("Example test", function(t) {
   t.pass();
   t.end();
 });
+
+
+test("Test Delete Function",function(t){
+
+  var obj=[{id:0 ,description:"Rana",done:true},{id:1,description:"yousef",done:true}];
+  var excpected=[{id:1 ,description:"yousef",done:true}];
+  var actual = logic.deleteTodo(obj,0);
+  t.deepEqual(actual,excpected,"when passing array ,Element should deleted");
+  t.end();
+})
