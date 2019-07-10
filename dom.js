321b6d6a82339c517f4931a53c6eb9101f25a396
rana <ranasobeid95@gmail.com>
2019-07-10 13:49:52 +0300

    remove comments

--- dom.js
+++ dom.js
@@ -3,51 +3,48 @@
 // it keeps everything inside hidden from the rest of our application
 (function() {
   // This is the dom node where we will keep our todo
-  var container = document.getElementById("todo-container");
-  var addTodoForm = document.getElementById("add-todo");
-  var state = [
+  let container = document.getElementById("todo-container");
+  let addTodoForm = document.getElementById("add-todo");
+  let state = [
     { id: -3, description: "first todo" },
     { id: -2, description: "second todo" },
     { id: -1, description: "third todo" }
   ]; // this is our initial todoList
-  var len=state.length;
+  let len = state.length;
 
-//////////////////////////header and date section/////////////////////////////
-let thebody=document.getElementsByTagName('body')[0];
-let headerPart=document.createElement('header');
-headerPart.setAttribute('class','header');
-let datePart=document.createElement('span');
-let today =new Date();
-option ={weekday:"long",month:"short",day:"numeric"};
-datePart.innerHTML=today.toLocaleDateString('en',option);
+  //////////////////////////header and date section/////////////////////////////
+  let thebody = document.getElementsByTagName("body")[0];
+  let headerPart = document.createElement("header");
+  headerPart.setAttribute("class", "header");
+  let datePart = document.createElement("span");
+  let today = new Date();
+  option = { weekday: "long", month: "short", day: "numeric" };
+  datePart.innerHTML = today.toLocaleDateString("en", option);
 
-datePart.setAttribute('class','date');
-let numTasks=document.createElement('span');
-numTasks.textContent=len+" task";
-numTasks.setAttribute('class','numTasks');
-headerPart.appendChild(datePart);
-headerPart.appendChild(numTasks);
-thebody.insertBefore(headerPart,container);
+  datePart.setAttribute("class", "date");
+  let numTasks = document.createElement("span");
+  numTasks.textContent = len + " task";
+  numTasks.setAttribute("class", "numTasks");
+  headerPart.appendChild(datePart);
+  headerPart.appendChild(numTasks);
+  thebody.insertBefore(headerPart, container);
 
-var taskElement=document.getElementsByClassName('numTasks')[0];
+  let taskElement = document.getElementsByClassName("numTasks")[0];
 
-
-
-// add sort button
-  var sortTasksButton = document.createElement('button');
-  sortTasksButton.classList.add = 'sort-button';
-  sortTasksButton.textContent = "Sort Tasks"
+  // add sort button
+  let sortTasksButton = document.createElement("button");
+  sortTasksButton.classList.add = "sort-button";
+  sortTasksButton.textContent = "Sort Tasks";
   headerPart.appendChild(sortTasksButton);
-  sortTasksButton.addEventListener('click', function(event) {
+  sortTasksButton.addEventListener("click", function(event) {
     let sortState = todoFunctions.sortTodos(state);
     update(sortState);
-    taskElement.textContent=sortState.length;
-  })
-
+    taskElement.textContent = sortState.length;
+  });
 
   // This function takes a todo, it returns the DOM node representing that todo
-  var createTodoNode = function(todo) {
-    var todoNode = document.createElement("li");
+  let createTodoNode = function(todo) {
+    let todoNode = document.createElement("li");
     // you will need to use addEventListener
 
     // add span holding description
@@ -57,28 +54,28 @@ var taskElement=document.getElementsByClassName('numTasks')[0];
     todoNode.appendChild(todoElement);
 
     // this adds the delete button
-    var deleteButtonNode = document.createElement("button");
-    var symbol = document.createElement("i");
+    let deleteButtonNode = document.createElement("button");
+    let symbol = document.createElement("i");
     symbol.className = "far fa-trash-alt";
     deleteButtonNode.appendChild(symbol);
     deleteButtonNode.addEventListener("click", function(event) {
-      var newState = todoFunctions.deleteTodo(state, todo.id);
+      let newState = todoFunctions.deleteTodo(state, todo.id);
       update(newState);
-      taskElement.textContent=newState.length;
+      taskElement.textContent = newState.length;
     });
     todoNode.appendChild(deleteButtonNode);
 
     // add markTodo button
 
     // add classes for css
-    var markedTodoButton = document.createElement("button");
-    var symbol = document.createElement("i");
+    let markedTodoButton = document.createElement("button");
+    let symbol = document.createElement("i");
     symbol.className = "far fa-check-circle";
     markedTodoButton.appendChild(symbol);
     markedTodoButton.addEventListener("click", function(event) {
-      var newState = todoFunctions.markTodo(state, todo.id);
+      let newState = todoFunctions.markTodo(state, todo.id);
       update(newState);
-      taskElement.textContent=newState.length;
+      taskElement.textContent = newState.length;
     });
     todoNode.appendChild(markedTodoButton);
 
@@ -91,33 +88,24 @@ var taskElement=document.getElementsByClassName('numTasks')[0];
       event.preventDefault();
       let todoContext = document.getElementsByName("description")[0].value;
       // validation for user -- can not enter spcial char
-      if(todoContext !== ""){
-      let newItem = todoFunctions.addTodo(state, todoContext);
-      taskElement.textContent=newItem.length;
-      document.getElementsByName("description")[0].value = "";
-      update(newItem);
-     
+      if (todoContext !== "") {
+        let newItem = todoFunctions.addTodo(state, todoContext);
+        taskElement.textContent = newItem.length;
+        document.getElementsByName("description")[0].value = "";
+        update(newItem);
       }
-      
-      // what does event.preventDefault do?
-      // what is inside event.target?
-
-      // var description = "?"; // event.target ....
-      // hint: todoFunctions.addTodo
-      // var newState = []; // ?? change this!
-      // update(newState);
     });
   }
 
   // you should not need to change this function
-  var update = function(newState) {
+  let update = function(newState) {
     state = newState;
     renderState(state);
   };
 
   // you do not need to change this function
-  var renderState = function(state) {
-    var todoListNode = document.createElement("ul");
+  let renderState = function(state) {
+    let todoListNode = document.createElement("ul");
 
     state.forEach(function(todo) {
       todoListNode.appendChild(createTodoNode(todo));
